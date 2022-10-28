080405f26f251d150827c043df63933a23472952
Sora210 <sokominto1231@gmail.com>
2022-10-29 01:44:25 +0900

    Create API Project

---
+++ api/index.js
@@ -0,0 +1,22 @@
+//#########################################
+const PORT = 3333;
+//#########################################
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//#########################################
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        'status': 'Error',
+        'message': 'Not Found'
+    })
+})
+//#########################################
+app.listen(PORT, () => {
+    console.log('Server Start!');
+    console.log('LISTEN PORT: ' + PORT);
+})
