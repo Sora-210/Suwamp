772af3a970e4f61547fcae2b15c732bf2363bbdb
Sora-210 <sokominto1231@gmail.com>
2022-10-29 22:25:13 +0900

    Update LoginHash of Login function

--- api/router/Login.js
+++ api/router/Login.js
@@ -1,13 +1,12 @@
 //#########################################
 const router = require('express').Router();
 const db = require('../db');
+const createQuery = require('../func/createQuery')
 const crypto = require('crypto')
 //#########################################
 
 router.post('/', async (req, res) => {
     try {
-        //Error
-        let error = null
         //BodyCheck
         if (!req.body['name']) throw new Error('BodyError:name');
         if (!req.body['password']) throw new Error('BodyError:password');
@@ -15,17 +14,31 @@ router.post('/', async (req, res) => {
         const passHash = crypto.createHash('sha256').update(Buffer.from(req.body['password'])).digest('hex');
 
         //Check Unique UserName
-        db.query(`SELECT COUNT(*) AS n FROM Users WHERE Name = "${req.body['name']}" AND Password = "${passHash}";`, (err, rows) => {
+        db.query(createQuery(
+            'SELECT COUNT(*) AS n FROM Users ',
+            `WHERE Name = "${req.body['name']}" AND Password = "${passHash}";`
+        ), (err, rows) => {
             if (err) throw err;
             if (rows[0].n === 1) {
-                res.status(200).json({
-                    "status": "success",
-                    "message": "Login"
+                //LoginHash
+                const loginHash = crypto.createHash('sha256').update(Buffer.from(req.body['name'] + Math.random().toString(32).substring(2) + (new Date()).toString())).digest('hex');
+                db.query(createQuery(
+                    'UPDATE Users SET ',
+                    `LoginHash = "${loginHash}" `,
+                    `WHERE Name = "${req.body['name']}";`
+                ), (err) => {
+                    if (err) throw err;
+
+                    res.status(200).json({
+                        "status": "success",
+                        "message": "Login",
+                        "LoginHash": loginHash
+                    })
                 })
             } else {
                 res.status(403).json({
                     "status": "Error",
-                    "message": "can't Login"
+                    "message": "Can't Login"
                 })
             }
         })
